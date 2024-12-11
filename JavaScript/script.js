fed2d18553d028fc422d45607f9337a3bfc48daf
hala <hala2000160@student.eelu.edu.eg>
2024-12-11 22:52:48 +0200

    first commit

---
+++ JavaScript/script.js
@@ -0,0 +1,22 @@
+var num1=Number(prompt("enter first num"))
+var num2=Number(prompt("enter second num"))
+
+ var sum
+ sum=num1+num2
+ console.log(sum);
+
+
+ var n1=15
+ var n2=3
+ console.log(n1+n2)
+ console.log(n1-n2)
+ console.log(n1*n2)
+ console.log(n1**n2)
+ console.log(n1/n2)
+ console.log(n1%n2)
+
+ 
+
+
+ 
+
